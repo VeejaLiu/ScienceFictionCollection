f689e40c0ea3bd7c01d6e3c56f5525c72b3c3505
VeejaLiu <veejaliu@gmail.com>
2025-02-25 09:43:22 +0800

    增强 convert.sh 脚本，添加打印函数以输出信息、警告和错误，并优化文件处理逻辑

--- convert.sh
+++ convert.sh
@@ -1,10 +1,37 @@
 #!/bin/bash
 
+# 打印函数, 绿色
+function print() {
+    echo -e "\033[32m[Info] $1\033[0m"
+}
+
+# 打印函数, 红色
+function print_error() {
+    echo -e "\033[31m[Error] $1\033[0m"
+}
+
+# 打印函数, 黄色
+function print_warn() {
+    echo -e "\033[33m[Warn] $1\033[0m"
+}
+
+
 # 遍历当前目录下的所有文件
 for file in *; do
+    # 输出文件名
+    print "File: $file"
+
+    # 文件名不是txt结尾的文件跳过
+    if [[ "${file##*.}" != "txt" ]]; then
+        print_warn "Not a txt file, skip."
+        continue
+    fi
+
     # 检查文件是否是常规文件
     if [[ -f "$file" ]]; then
         # 使用iconv转换编码并保存到临时文件
-        iconv -f gbk -t utf-8 "$file" > temp.txt && mv temp.txt "$file"
+        iconv -f gbk -t utf-8 "$file" > temp.txt
+        # 替换原文件
+        mv temp.txt "$file"
     fi
 done
